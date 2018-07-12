1a7c577b3944d93672fe531fdbe387cf116bc7c4
Nattapon Benz <sevipsagis@gmail.com>
2018-07-12 11:43:37 +0700

    add checked function

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var playerChance = 5;
-$(document).ready(function(){
-    
+var playerChance = 7;
+var gameStatus = false;
+$(document).ready(function() {
+    var randomNumber = Math.floor(Math.random()*100);
+    console.log(randomNumber);
+
+    $(checkedButton).on("click", function(){
+        var playerNumber = parseInt(gameInput.value);
+        console.log(playerNumber);
+        if(randomNumber > playerNumber){
+            gameTitle.innerHTML = "It's too low."
+            playerChance--;
+        }
+        else if(randomNumber < playerNumber){
+            gameTitle.innerHTML = "It's too high."
+            playerChance--;
+        }
+        else{
+            gameTitle.innerHTML = "It's corrected."
+            gameStatus = true;
+        }
+    });
 });
