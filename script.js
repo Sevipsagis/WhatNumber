d40d24cbb9fbae996e67e034c5420d4bb8462915
Nattapon Benz <sevipsagis@gmail.com>
2018-07-12 11:48:09 +0700

    add empty input and game end condition

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 var playerChance = 7;
 var gameStatus = false;
-$(document).ready(function() {
-    var randomNumber = Math.floor(Math.random()*100);
+$(document).ready(function () {
+    var randomNumber = Math.floor(Math.random() * 100);
     console.log(randomNumber);
 
-    $(checkedButton).on("click", function(){
-        var playerNumber = parseInt(gameInput.value);
-        console.log(playerNumber);
-        if(randomNumber > playerNumber){
-            gameTitle.innerHTML = "It's too low."
-            playerChance--;
-        }
-        else if(randomNumber < playerNumber){
-            gameTitle.innerHTML = "It's too high."
-            playerChance--;
-        }
-        else{
-            gameTitle.innerHTML = "It's corrected."
-            gameStatus = true;
+    $(checkedButton).on("click", function () {
+        if (gameStatus != true && playerChance != 0 && gameInput.value != "") {
+            var playerNumber = parseInt(gameInput.value);
+            console.log(playerNumber);
+            if (randomNumber > playerNumber) {
+                gameTitle.innerHTML = "It's too low."
+                playerChance--;
+            }
+            else if (randomNumber < playerNumber) {
+                gameTitle.innerHTML = "It's too high."
+                playerChance--;
+            }
+            else {
+                gameTitle.innerHTML = "It's corrected."
+                gameStatus = true;
+            }
         }
     });
 });
