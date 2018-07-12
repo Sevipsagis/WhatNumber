0ff7e73162d7af31b93a50241cedec9edad5f4c0
Nattapon Benz <sevipsagis@gmail.com>
2018-07-12 13:07:01 +0700

    add alert

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var playerChance = 7;
 var gameStatus = false;
+yourChance.innerHTML = playerChance;
 $(document).ready(function () {
     var randomNumber = Math.floor(Math.random() * 100);
     console.log(randomNumber);
@@ -11,15 +12,18 @@ $(document).ready(function () {
             if (randomNumber > playerNumber) {
                 gameTitle.innerHTML = "It's too low."
                 playerChance--;
+                gameAlert.classList.toggle("d-none");
             }
             else if (randomNumber < playerNumber) {
                 gameTitle.innerHTML = "It's too high."
                 playerChance--;
+                gameAlert.classList.toggle("d-none");
             }
             else {
                 gameTitle.innerHTML = "It's corrected."
                 gameStatus = true;
             }
+            yourChance.innerHTML = playerChance;
         }
     });
 });
