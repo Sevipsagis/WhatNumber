cb3c32ab27320d393ac4a278201c80a388d0fc7e
Nattapon Benz <sevipsagis@gmail.com>
2018-07-12 13:29:55 +0700

    develop alert function

--- script.js
+++ script.js
@@ -12,18 +12,41 @@ $(document).ready(function () {
             if (randomNumber > playerNumber) {
                 gameTitle.innerHTML = "It's too low."
                 playerChance--;
-                gameAlert.classList.toggle("d-none");
+                showAlert(1);
             }
             else if (randomNumber < playerNumber) {
                 gameTitle.innerHTML = "It's too high."
                 playerChance--;
-                gameAlert.classList.toggle("d-none");
+                showAlert(2);
             }
             else {
                 gameTitle.innerHTML = "It's corrected."
                 gameStatus = true;
+                showAlert(0);
             }
             yourChance.innerHTML = playerChance;
         }
     });
+    function showAlert(status){
+        gameAlert.classList.toggle("d-none");
+        if(status==0){
+            gameAlert.classList.add("alert-success");
+            gameAlert.innerHTML = "Correct!!!"
+
+        }
+        else if(status==1){
+            gameAlert.classList.add("alert-danger");
+            gameAlert.innerHTML = "It's too low!!!"
+        }
+        else{
+            gameAlert.classList.add("alert-danger");
+            gameAlert.innerHTML = "It's too high!!!"
+        }
+        hideAlert();
+    }
+    function hideAlert(){
+        setTimeout(function(){
+            gameAlert.classList.toggle("d-none");
+        }, 2000);
+    }
 });
